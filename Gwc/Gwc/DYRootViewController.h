f6c82608bc51fbca13e3d7d63a7998de0a94649e
apple <1396539283@qq.com>
2016-08-26 14:41:17 +0800

    初始化配置

---
+++ Gwc/Gwc/DYRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  DYRootViewController.h
+//  Gwc
+//
+//  Created by apple on 8/26/16.
+//  Copyright © 2016 com.dayangdata.gwc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DYRootViewController : UIViewController
+
+@end
