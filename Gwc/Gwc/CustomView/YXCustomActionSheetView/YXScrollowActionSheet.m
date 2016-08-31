935b137178b340aea9f3fb67427e195583ddb050
apple <1396539283@qq.com>
2016-08-31 16:48:17 +0800

    命名重复的问题

--- Gwc/Gwc/CustomView/YXCustomActionSheetView/YXScrollowActionSheet.m
+++ Gwc/Gwc/CustomView/YXCustomActionSheetView/YXScrollowActionSheet.m
@@ -149,6 +149,13 @@
     
     [self tapBg];
 }
+/* 数组格式
+NSArray *contentArray = @[@{@"name":@"新浪微博",@"icon":@"sns_icon_3"},
+                          @{@"name":@"QQ空间 ",@"icon":@"sns_icon_5"},
+                          @{@"name":@"QQ ",@"icon":@"sns_icon_4"},
+                          @{@"name":@"微信",@"icon":@"sns_icon_7"},
+                          @{@"name":@"朋友圈",@"icon":@"sns_icon_8"},
+                          @{@"name":@"微信收藏",@"icon":@"sns_icon_9"}];
 
-
+*/
 @end
